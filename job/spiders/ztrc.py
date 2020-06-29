8246cf7f4e10bfb0f1c67128ee40d256fb53aee5
lkds <2576027877@qq.com>
2020-06-29 20:56:51 +0800

    智通人才网初次提交

---
+++ job/spiders/ztrc.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import items.JobItem
+
+class ZtrcSpider(scrapy.Spider):
+    name = 'ztrc'
+    allowed_domains = ['www.job5156.com']
+    start_urls = ['http://www.job5156.com/']
+
+    def parse(self, response):
+        # item = JobItem()
+
+        # for post in response.xpath("//article/div"):
+        #     # print(p)
+        #     item['Jname'] = post.xpath(
+        #         './header/h1/a/text()').extract()[0].strip()
+        #     item['Jsalary'] = 0
+        #     item['Jarea'] = ''
+        #     item['Jtype'] = post.xpath('./div/text()').extract()[0].strip()
+        #     item['Jrequirements'] = ''
+        #     item['Jcompany'] = ''
+        #     item['Jtag'] = ''
+        #     item['Jwelfare']=''
+        #     yield item
+        pass
