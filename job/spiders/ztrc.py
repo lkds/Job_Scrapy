8b866ca7bae373da891922196d656dd4b553de6e
lkds <2576027877@qq.com>
2020-07-01 09:47:32 +0800

    尝试修复大街网，无果

--- job/spiders/ztrc.py
+++ job/spiders/ztrc.py
@@ -5,21 +5,21 @@ from job.items import JobItem
 class ZtrcSpider(scrapy.Spider):
     name = 'ztrc'
     allowed_domains = ['www.job5156.com']
-    start_urls = ['http://www.job5156.com/']
+    start_urls = ['http://www.job5156.com/s/result?t=1593527827158&keyword=&keywordType=&posTypeList=&locationList=&taoLabelList=&degreeFrom=&propertyList=&industryList=&sortBy=0&urgentFlag=&comIdList=&maxSalary=&salary=&workyearFrom=&workyearTo=&degreeTo=&pn=1']
 
     def parse(self, response):
-        # item = JobItem()
+        item = JobItem()
 
-        # for post in response.xpath("//article/div"):
-        #     # print(p)
-        #     item['Jname'] = post.xpath(
-        #         './header/h1/a/text()').extract()[0].strip()
-        #     item['Jsalary'] = 0
-        #     item['Jarea'] = ''
-        #     item['Jtype'] = post.xpath('./div/text()').extract()[0].strip()
-        #     item['Jrequirements'] = ''
-        #     item['Jcompany'] = ''
-        #     item['Jtag'] = ''
-        #     item['Jwelfare']=''
-        #     yield item
+        for post in response.xpath("//article/div"):
+            # print(p)
+            item['Jname'] = post.xpath(
+                './header/h1/a/text()').extract()[0].strip()
+            item['Jsalary'] = 0
+            item['Jarea'] = ''
+            item['Jtype'] = post.xpath('./div/text()').extract()[0].strip()
+            item['Jrequirements'] = ''
+            item['Jcompany'] = ''
+            item['Jtag'] = ''
+            item['Jwelfare']=''
+            yield item
         pass
